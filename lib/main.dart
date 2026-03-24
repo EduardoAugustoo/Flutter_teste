cfa06988f12e069665d22b78ed32ef4a8c2f45f5
EduardoAugustoo <eduardodudu7777@gmail.com>
2026-03-23 21:41:41 -0300

    23032026

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
-        body: Text("teste")
-      )
-    )
+        appBar: AppBar(
+          backgroundColor: Color.fromRGBO(255, 176, 102, 1089),
+          title: Center(child: Text("Nome do meu app")),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center, 
+            children: [Text("Olá, mundo!"), Text("23/03/2026")]),
+        ),
+      ),
+    ),
   );
 }
