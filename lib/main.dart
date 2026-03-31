a82ef407215455331118d13ec42602f65de9480f
EduardoAugustoo <eduardodudu7777@gmail.com>
2026-03-30 21:37:31 -0300

    30032026

--- lib/main.dart
+++ lib/main.dart
@@ -3,17 +3,51 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
+  runApp(MeuApp());
+}
+
+class MeuApp extends StatelessWidget {
+  MeuApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromRGBO(255, 176, 102, 1089),
+          backgroundColor: Color.fromRGBO(255, 176, 102, 189),
           title: Center(child: Text("Nome do meu app")),
         ),
         body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center, 
-            children: [Text("Olá, mundo!"), Text("23/03/2026")]),
+          child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [Text("Olá, mundo!"), Text("23/03/2026")]),
+          child: Row(mainAxisAlignment: MainAxisAlignment.center, children: [Text("Olá, mundo!"), Icon(Icons.add)]),
         ),
       ),
     ),
   );
+    );
+  }
+}
+
+class PaginaInicial extends StatefulWidget {
+  @override
+  State<PaginaInicial> createState() => PaginaInicialState();
+}
+
+class PaginaInicialState extends State<PaginaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        children: [
+          Text(texto),
+          ElevatedButton(
+            child: Text('mudar texto'),
+            onPressed: () {
+              texto = "texto alterado";
+            },
+          ),
+        ],
+      ),
+    );
+  }
 }
